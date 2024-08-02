a786433ab28283804086d2dbf55e90f3cb115450
JavierCisneros <fjca185@gmail.com>
2024-08-02 09:43:43 -0600

    dropdown menu esc added

--- src/components/MenuDropDownMenu.tsx
+++ src/components/MenuDropDownMenu.tsx
@@ -1,20 +1,29 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function MenuDropDownMenu() {
   const dropdown = useRef<HTMLDivElement>(null);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   function handleClickOutside(event: MouseEvent) {
     if (dropdown.current && !dropdown.current.contains(event.target as Node)) {
-      dropdown.current.classList.add("hidden");
+      setDropdownVisible(false);
+    }
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key === "Escape") {
+      setDropdownVisible((prev) => !prev);
     }
   }
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
 
@@ -25,15 +34,10 @@ export default function MenuDropDownMenu() {
     >
       <button
         type="button"
-        onClick={() => {
-          const dropdownElement = document.getElementById("dropdown");
-          if (dropdownElement) {
-            dropdownElement.classList.toggle("hidden");
-          }
-        }}
-        className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 "
+        onClick={() => setDropdownVisible((prev) => !prev)}
+        className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50"
         id="menu-button"
-        aria-expanded="true"
+        aria-expanded={isDropdownVisible}
         aria-haspopup="true"
       >
         <div className="flex items-center justify-center rounded-full w-10 h-10 bg-purple_sensedia text-white">
@@ -55,7 +59,9 @@ export default function MenuDropDownMenu() {
       </button>
       <div
         ref={dropdown}
-        className="absolute bg-first_header border-[2px] hidden mt-2 origin-top-right w-auto text-white"
+        className={`absolute bg-first_header border-[2px] mt-2 origin-top-right w-auto text-white ${
+          isDropdownVisible ? "" : "hidden"
+        }`}
         id="dropdown"
       >
         <div className="cursor-pointer hover:bg-gray-500 p-4 hover:border-l-2 hover:border-purple_sensedia hover:border-solid">
