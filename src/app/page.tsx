3441005af221c3c895547324b75f221fdcb86434
JavierCisneros <fjca185@gmail.com>
2024-08-04 17:19:39 -0600

    added visual changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="bg-white h-full w-full md:w-3/4 lg:w-2/3 sticky flex items-center justify-center mx-auto flex-col md:flex-row pt-6">
+      <main className="bg-white h-full w-full md:w-3/4 lg:w-2/3 sticky flex items-center justify-center mx-auto flex-col md:flex-row pt-6 pb-12">
         <Link
           href="/users"
-          className=" flex items-center justify-center flex-col w-full md:w-1/2 lg:w-2/6 rounded-lg border-solid border-2 border-gray-300 h-64 mx-4 my-4 hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+          className=" flex items-center justify-center flex-col w-2/3 md:w-1/2 lg:w-2/6 rounded-lg border-solid border-2 border-gray-300 h-80 mx-4 my-4  hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ export default function Home() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="text-purple_sensedia w-12 h-12 mb-6"
+            className="text-purple_sensedia w-12 h-12 mb-6  mt-6"
           >
             <path d="M15 3v18" />
             <rect width="18" height="18" x="3" y="3" rx="2" />
@@ -29,7 +29,7 @@ export default function Home() {
             <path d="M21 15H3" />
           </svg>
           <h2 className="text-xl">Users</h2>
-          <p className="px-6 md:px-10 text-justify">
+          <p className="px-6 md:px-10 text-justify md:text-sm sm:py-8 sm:h-38">
             Here you can find the users list, search for any user by one of
             their fields, click on them and it will take you to the user
             profile, also you can delete any user.
@@ -37,7 +37,7 @@ export default function Home() {
         </Link>
         <Link
           href="/users/new"
-          className="flex items-center justify-center flex-col w-full md:w-1/2 lg:w-2/6 rounded-lg border-solid border-2 border-gray-300 h-64 mx-4 my-4 hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+          className="flex items-center justify-center flex-col w-2/3 md:w-1/2 lg:w-2/6 rounded-lg border-solid border-2 border-gray-300 h-80 mx-4 my-4 hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -49,13 +49,13 @@ export default function Home() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="text-purple_sensedia w-12 h-12 mb-6"
+            className="text-purple_sensedia w-12 h-12 mb-6 mt-6"
           >
             <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
             <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
           </svg>
           <h2 className="text-xl">Registry</h2>
-          <p className="px-6 md:px-10 text-justify">
+          <p className="px-6 md:px-10 text-justify md:text-sm sm:py-8 sm:h-38">
             Here you can add a new user to the list, just fill the fields after
             clicking on the submit button it will take you to the users table.
           </p>
